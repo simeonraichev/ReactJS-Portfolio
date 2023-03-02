8b41b59d7239b3b3d11ec963297bb0b6869fb99e
Simeon Raychev <simeon0022@gmail.com>
2023-03-02 22:28:50 +0200

    Updated banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,11 +12,11 @@ import {fadeIn} from '../variants';
 
 const Banner = () => {
   return (
-  <div className='section' id='home'>
+  <div className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
     <div className='container mx-auto'>
-      <div>
-        <div>
-          <h1>SIMEON <span>RAYCHEV</span></h1>
+      <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+        <div className='flex-1 text-center font-secondary lg:text-left'>
+          <h1 className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>SIMEON <span>RAYCHEV</span></h1>
           <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
             <span className='text-white mr-4'>I am a </span>
             <TypeAnimation sequence={['Student',2000, 'Developer',2000, 'Trader',2000]} 
@@ -25,7 +25,7 @@ const Banner = () => {
               wrapper='span'
               repeat={Infinity}/>
           </div>
-          <p className='mx-8-w-lg mx-auto lg:mx-0'>A passionate frontend and backend student from Sofia, Bulgaria.</p>
+          <p className='mb-8 max-w-lg mx-auto lg:mx-0'>A passionate frontend and backend student from Sofia, Bulgaria.</p>
           <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
             <button className='btn btn-lg'>Contact me</button>
             <a href='#' className='text-gradient btn-link'>My Portfolio</a>
