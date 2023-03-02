b47ddf0586e056da0ec06e49fb324ef8b3364f03
Simeon Raychev <simeon0022@gmail.com>
2023-03-02 21:55:11 +0200

    Header bug fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BsListNested } from 'react-icons/bs';
+
+import Logo from '../assets/logo.svg';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+  <header className='py-8'>
+      <div className='container max-auto'>
+        <div className='flex justify-between items-center'>
+          <a href='#'>
+            <img src={Logo} alt=""></img>
+          </a>
+          <button className='btn btn-sm'>Work with me</button>
+        </div>
+      </div>
+    </header>
+    );
 };
 
 export default Header;
